3ae6b1eb77a8dacc85f2ca54cb795c55440a9cf4
Arthur DaVega <arthurkdavega@gmail.com>
2021-12-14 23:05:19 -0500

    Padding out search function, removed cli file name from find output. Both functions return same output.

--- FindZNNCLI.py
+++ FindZNNCLI.py
@@ -3,36 +3,38 @@ import subprocess as sp
 import sys
 from typing import Union
 
-
+#find is expensive so see if it's been added to path first
+#need to research CLI commands for all possible sys.platform values and add to pathcode
 def search(pathVariable: str) -> int:
-    platform = sys.platform
-    if platform == 'win32':
-        command = 'echo %PATH%'
-    elif platform in ('linux', 'darwin'):
-        command = 'echo $Path'
-    else:
-        return -1
-    position = sp.getoutput(command).find(pathVariable)
-    return position
+    pathcode = {'win32': 'echo %PATH%', 'linux': 'echo $Path', 'darwin': 'echo $Path'}
+    output = sp.getoutput(pathcode[sys.platform])
+    position = output.find(pathVariable)
+    if position == -1:
+        return position
+    pathBeginIndex = output[:position].rfind(';')+1
+    pathEndIndex = output.find(';', position)
+    pathToZNN = output[pathBeginIndex:pathEndIndex]
+    return pathToZNN
 
-
+#Find searches C for Windows and root for Mac/Linux
 def find(name: str, path: str) -> Union[int, str]:
     for root, dirs, files in os.walk(path):
         if name in files:
-            return os.path.join(root, name)
+            return os.path.join(root + os.path.sep)
 
 
 def findZNN():
-    pathindex = search('Zenon')
-    if pathindex == -1:
+    pathtoZNN = search('Zenon')
+    if pathtoZNN == -1:
         if sys.platform == 'win32':
-            pathindex = find('znn-cli.exe', 'C:\\')
+            pathtoZNN = find('znn-cli.exe', 'C:\\')
         elif sys.platform in ('linux', 'darwin'):
-            pathindex = find('znn-cli.exe', '\\')
-    if pathindex in [-1, None]:
+            pathtoZNN = find('znn-cli.exe', '\\')
+    if not pathtoZNN:
         return False
     else:
-        return pathindex.replace('.exe', '')
+        return pathtoZNN
 
 if __name__ == '__main__':
+    print(search('Zenon'))
     print(find('znn-cli.exe', 'C:\\Users\\'))
